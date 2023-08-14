41f64b9929ccdf4fa0dcbb2acc5adbfce74c9e74
yeonnex <yeonnex@gmail.com>
2023-08-14 23:12:08 +0900

    feat: JobInstance 도메인 이해 - 스프링부트의 자동 잡 실행 제거 - ApplicationRunner 를 상속받은 사용자 정의 JobRunner 클래스로 잡 실행

---
+++ src/main/java/com/example/hellobatch/JobRunner.java
@@ -0,0 +1,30 @@
+package com.example.hellobatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JobRunner implements ApplicationRunner {
+
+    @Autowired
+    JobLauncher jobLauncher;
+    @Autowired
+    Job job;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addDate("date", new Date())
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+    }
+}
