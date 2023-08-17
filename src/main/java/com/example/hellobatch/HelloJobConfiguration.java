8e62f0d8d1e07a468fdc54a0c9ad1fabee0ee355
yeonnex <yeonnex@gmail.com>
2023-08-17 23:52:19 +0900

    feat: StepContribution 도메인 이해 - 람다에서 익명클래스로 Tasklet 정의

--- src/main/java/com/example/hellobatch/HelloJobConfiguration.java
+++ src/main/java/com/example/hellobatch/HelloJobConfiguration.java
@@ -4,8 +4,11 @@ import com.example.hellobatch.tasklet.CustomTasklet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,11 +37,13 @@ public class HelloJobConfiguration {
     @Bean
     Step helloJobStep2() {
         return stepBuilderFactory.get("helloStep2")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("===================");
-                    System.out.println(" >> 안녕, 스프링 배치 step 2");
-                    System.out.println("===================");
-                    return RepeatStatus.FINISHED;
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        contribution.getStepExecution().getJobExecution().getJobInstance().getJobName();
+                        System.out.println(" >> 안녕, 스프링 배치 step 2");
+                        return RepeatStatus.FINISHED;
+                    }
                 }).build();
     }
 }
